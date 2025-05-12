726b4bb50c8e919f520f8aad5acb9fdf382e5e31
Sagar <binnu.sagaraggrawal@gmail.com>
2025-05-12 21:20:01 +0530

    init commit

--- src/java_collection_code/List_ques.java
+++ src/java_collection_code/List_ques.java
@@ -1,6 +1,8 @@
 package java_collection_code;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class List_ques {
 
@@ -15,6 +17,17 @@ public class List_ques {
             else
                 System.out.println("odd no: " + i);
         });
+
+        System.out.println("////////////////////////////////////////////////////////");
+
+        List<Integer> lt2 = List.of(2,1,7,18,21,34,94,53,12,45,67,89,90,90,34,45,67,89,89,67,45,3,2,1);
+        List<Integer> lt3 = lt2.stream().distinct().sorted().collect(Collectors.toList());
+        System.out.println("Distinct and sorted list in ascending order: " + lt3);
+        System.out.println("////////////////////////////////////////////////////////");
+        System.out.println("////////////////////////////////////////////////////////");
+        List<Integer> lt4 = lt2.stream().distinct().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        System.out.println("Distinct and sorted list in Descending order: " + lt4);
+
 
     }
 }
