fee0fa576f6904bbb1b8468579ab99322ca5abc1
Sagar <binnu.sagaraggrawal@gmail.com>
2025-05-12 21:57:54 +0530

    init commit

---
+++ src/java_collection_code/str_ques.java
@@ -0,0 +1,22 @@
+package java_collection_code;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class str_ques {
+    public static void main(String[] args) {
+
+       String str = "Java is a programming language";
+
+       String result = str.replaceAll("[aeiouAEIOU]", "");
+       System.out.println("String after removing vowels: " + result);
+
+       String result1 = str.replaceAll("[^aeiouAEIOU]", "");
+       System.out.println("String after removing Constants: " + result1);
+
+       String result2 = Arrays.stream(str.split(" "))
+               .map(word -> new StringBuilder(word).reverse().toString())
+               .collect(Collectors.joining(" "));
+         System.out.println("String after reversing each word: " + result2);
+    }
+}
